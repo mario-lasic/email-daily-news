b32d45895b4ab71d072d6a90df2a25a637204c14
Mario Lasić <mario.lasic@outlook.com>
2024-10-23 17:29:27 +0200

    Get title and description from articles

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
 from password import api, email_password
 
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-09-22&sortBy=publishedAt&apiKey={
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-09-25&sortBy=publishedAt&apiKey={
     api}"
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the articles titles and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
+
